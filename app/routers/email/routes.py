b61e170a89f25fa77950f61a85b9c11af61ea5e5
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2026-02-14 19:41:10 +0800

    add email routes folder

---
+++ app/routers/email/routes.py
@@ -0,0 +1,5 @@
+# Email Routes
+
+# Route::post('/email-all-faculty-pref-submitted', [EmailController::class, 'emailPrefSubmitted']);
+# Route::post('/email-all-faculty-schedule', [EmailController::class, 'emailAllFacultySchedule']);
+# Route::post('/email-single-faculty-schedule', [EmailController::class, 'emailSingleFacultySchedule']);
