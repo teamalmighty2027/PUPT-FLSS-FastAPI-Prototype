5422ac7e3926bcb5e8042edc76cd7b5037b36a2f
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2026-02-14 19:38:28 +0800

    add course routes folder

---
+++ app/routers/course/routes.py
@@ -0,0 +1,6 @@
+# Course Routes
+
+# Route::get('/courses', [CourseController::class, 'index']);
+# Route::post('/addCourse', [CourseController::class, 'addCourse']);
+# Route::put('/courses/{id}', [CourseController::class, 'updateCourse']);
+# Route::delete('/courses/{id}', [CourseController::class, 'deleteCourse']);
