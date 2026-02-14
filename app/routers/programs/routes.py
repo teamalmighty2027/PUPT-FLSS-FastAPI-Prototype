8eee7b981ca3543f47cf525abc8174a9f7774a8b
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2026-02-14 19:54:56 +0800

    add programs routes folder

---
+++ app/routers/programs/routes.py
@@ -0,0 +1,7 @@
+# Programs Routes
+
+# Route::get('/programs', [ProgramController::class, 'getPrograms']);
+# Route::post('/addProgram', [ProgramController::class, 'addProgram']);
+# Route::get('/programs/{id}', [ProgramController::class, 'getProgramDetails']);
+# Route::put('/updateProgram/{id}', [ProgramController::class, 'updateProgram']);
+# Route::delete('/deleteProgram/{id}', [ProgramController::class, 'deleteProgram']);
