a3742a2960890f4471137be6df9abeedd5cebdeb
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2026-02-14 19:12:17 +0800

    rename directories

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from .routers import router as api_router
 
 app = FastAPI()
+app.include_router(api_router)
 
 @app.get("/")
 async def root():
