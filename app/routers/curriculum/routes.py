d48a4889efd03c3aebd295e0ec52b60819cf9221
bautista-johnmatthew <johnmatthewbautista01@gmail.com>
2026-02-14 19:39:46 +0800

    add curriculum routes folder

---
+++ app/routers/curriculum/routes.py
@@ -0,0 +1,14 @@
+# Curriculum & Curriculum Details Routes
+
+# Route::get('/curricula', [CurriculumController::class, 'index']);
+# Route::get('/curricula/{id}', [CurriculumController::class, 'show']);
+# Route::post('/addCurriculum', [CurriculumController::class, 'addCurriculum']);
+# Route::post('/deleteCurriculum', [CurriculumController::class, 'deleteCurriculum']);
+# Route::post('/copyCurriculum', [CurriculumController::class, 'copyCurriculum']);
+# Route::put('/updateCurriculum/{id}', [CurriculumController::class, 'update']);
+
+# Route::post('/removeProgramFromCurriculum', [CurriculumController::class, 'removeProgramFromCurriculum']);
+# Route::get('/programs-by-curriculum-year/{curriculumYear}', [CurriculumController::class, 'getProgramsByCurriculumYear']);
+# Route::post('/addProgramToCurriculum', [CurriculumController::class, 'addProgramToCurriculum']);
+
+# Route::get('/curricula-details/{curriculumYear}/', [CurriculumDetailsController::class, 'getCurriculumDetails']);
